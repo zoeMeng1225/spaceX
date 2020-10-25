cb9575329a4864cbe6774971022177bbd0bd486e
Zoe Meng <zoebo1225@gmail.com>
2020-10-25 11:17:23 -0700

    modified

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 
 function Header(){
   return(
     <div className = "header-wrapper">
-      <span className = "header-span">
-      <img src= {require('../assets/space_logo.svg')} alt=""/>
-       </span>
+
+      <Link to = "/">
+        <span className = "header-span">
+          <img src= {require('../assets/space_logo.svg')} alt=""/>
+        </span>
+      </Link>
     </div>
     
   )
